a6b88093e830818f08cb81ae2e79d0942c16962f
RCOliveira98 <rcoliveira.ti@gmail.com>
2021-06-25 23:05:30 -0300

    atualiza banner

--- src/app/components/banner/banner.component.ts
+++ src/app/components/banner/banner.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { IImage } from 'src/app/shared/models/image.model';
+
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BannerComponent implements OnInit {
 
+  images: IImage[];
+
   constructor() { }
 
   ngOnInit(): void {
+    this._carregarImagens();
+  }
+
+  private _carregarImagens(): void {
+    this.images = [
+      {url: '../../../assets/image/banner/foto1.jpg'},
+      {url: '../../../assets/image/banner/fotao2.jpg'},
+      {url: '../../../assets/image/banner/foto3.jpg'}
+    ]
   }
 
 }
